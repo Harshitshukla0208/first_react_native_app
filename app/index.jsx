6d38c8c18b5b16cd57c7c1cb32796da8fc7e06ba
Harshit Shukla <mrharshit2409@gmail.com>
2024-05-24 21:32:13 +0530

    configured tailwind and added a page using expo-router

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+    return (
+    <View className="flex-1 items-center justify-center bg-black">
+        <Text className="text-1xl text-white">Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+    </View>
+    );
+}
